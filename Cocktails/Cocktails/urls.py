48111b3763875189216063dfd3d77d6a4e0693c3
Vic <avigdorbrandt@gmail.com>
2022-06-03 17:35:18 +0300

    Added autodocumentation

--- Cocktails/Cocktails/urls.py
+++ Cocktails/Cocktails/urls.py
@@ -4,11 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/counter/', include('counter.urls')),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
