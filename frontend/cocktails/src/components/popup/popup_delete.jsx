7fa05761b3cb5457feb062d157464835cd1cf14d
Vic <avigdorbrandt@gmail.com>
2022-12-26 13:18:55 +0200

    fixet button service row

--- frontend/cocktails/src/components/popup/popup_delete.jsx
+++ frontend/cocktails/src/components/popup/popup_delete.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { RegularButton } from "../buttons/regular_button";
+import {ReactComponent as CloseIcon} from "../../svg/close_icon.svg"
+import '../../styles/popup_delete.scss'
 
 export function PopupDelete({
     subject, 
@@ -9,27 +11,28 @@ export function PopupDelete({
     }){
 
     return(
-        <div className={delete_active ? 'popup_wrapper active' : 'popup_wrapper'} onClick={()=>setDelete_active(false)}>
-            <div className="popup_content" onClick={e => e.stopPropagation()}>
-               <div  className="popup_delete_content"> 
-                    <div className="popup_title">
+        <div className={delete_active ? 'popup_mobile_wrapper active' : 'popup_mobile_wrapper'} onClick={()=>setDelete_active(false)}>
+            <div className="popup_mobile_content delete_popup" onClick={e => e.stopPropagation()}>
+                <div className="popup_filter_sevice_button_wrapper">
+                    <div className="popup_filter_close_button" onClick={()=>{setDelete_active(false)}}>
+                        <CloseIcon className='close_button_icon'/>
+                    </div> 
+                </div>
+                    <div className="popup_title delete_popup">
                         Are you sure
                     </div>
                 
-                    <div className="popup_delete_masssege regular_text">
-                        delete {subject}?
+                    <div className="popup_delete_masssege">
+                        Delete {subject}?
                     </div>
                     <div className="popup_delete_button_line">
-                        <div onClick={()=>setDelete_active(false)}>
+                        <div className="popup_delete_button_wrpper" onClick={()=>setDelete_active(false)}>
                             <RegularButton lable={'No'}></RegularButton>
                         </div>  
-                        <div onClick={func}>
+                        <div className="popup_delete_button_wrpper" onClick={func}>
                             <RegularButton lable={'Yes'}></RegularButton>
                         </div> 
-                    </div>
-                       
-                </div>
-                                    
+                    </div>          
             </div>
         </div>
     )
