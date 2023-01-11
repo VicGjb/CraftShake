53506327b90755c4e2685b4e70ff89a47780604f
Vic <avigdorbrandt@gmail.com>
2023-01-11 15:47:09 +0200

    Restyle place subtitle

--- frontend/cocktails/src/components/place_subtitle.jsx
+++ frontend/cocktails/src/components/place_subtitle.jsx
@@ -5,23 +5,24 @@ export const PlaceSubtitle=({place})=>{
 
     return(
         <div className="place_detaile_subtitle_wrap">
+            <div className="place_detaile_subtitle__requisites___slot address">
+                <div className="place-detaile-subtitle__addres">
+                    <p>{place.address}</p>
+                </div>
+            </div>
             <div className="place_detaile_subtitle_name" key={place.name}>
                 <div className="place_detaile__name">
                     <p>{place.name}</p>
                 </div>
-            </div>
-                
-            <div className="place_detaile_subtitle_requisites">
-                <div className="place_detaile_subtitle__requisites___slot">
-                    <p>{place.address}</p>
-                </div>
-                <div className="place_detaile_subtitle__requisites___slot">
+                <div className="place_detaile_subtitle__requisites___slot phone">
                     <p>{place.phone}</p>
                 </div>
-                <div className="place_detaile_subtitle__requisites___slot" >
-                    <p>{place.email}</p>
-                </div>
+            </div>
+            <div className="place_detaile_subtitle__requisites___slot email" >
+                    <p className="email_row">{place.email}</p>
+                    <p className="phone_row">{place.phone}</p>
             </div>
         </div>
+        
     )
 }
