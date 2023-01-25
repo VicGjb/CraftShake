f9530114e4b442863575041ea6de59c622065ee5
Vic <avigdorbrandt@gmail.com>
2023-01-25 19:35:55 +0200

    try fix static

--- Cocktails/Cocktails/settings.py
+++ Cocktails/Cocktails/settings.py
@@ -195,12 +195,12 @@ AWS_QUERYSTRING_AUTH  = False
 STATICFILES_FINDERS = [
     'compressor.finders.CompressorFinder',
 ]
-STATIC_URL = '/static/'
+STATIC_URL = '/django/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_DIR=os.path.join(BASE_DIR, 'static/files')
 STATICFILES_DIRS=[STATIC_DIR]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/django/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
