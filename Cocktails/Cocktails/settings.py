420606328709b8cd4bd4b8212417e8e78fba800b
Vic <avigdorbrandt@gmail.com>
2023-01-31 17:46:21 +0200

    123

--- Cocktails/Cocktails/settings.py
+++ Cocktails/Cocktails/settings.py
@@ -1,9 +1,10 @@
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
 if os.environ.get('ENVIRONMENT') == 'local':
     print('local')
     from .settings_development import *
 elif os.environ.get('ENVIRONMENT') == 'production':
     print('Prod')
     from .settings_production import *
+else:
+    print('LOCAL FORCE')
+    from .settings_development import *
