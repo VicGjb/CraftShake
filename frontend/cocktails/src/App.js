83b8e08b3a2e518826c25b5dd040ec684027558e
Vic <avigdorbrandt@gmail.com>
2023-02-01 15:11:02 +0200

    clean some things in APP.js

--- frontend/cocktails/src/App.js
+++ frontend/cocktails/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Routing } from './components/router';
-import {RouteChangeTracker} from './components/router';
 import ReactGA from 'react-ga';
-import RouteMiddleware from './components/RouteMiddleware';
+
 
 let TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID; 
 ReactGA.initialize(TRACKING_ID);
