7dac96431163edb683cbcf880226c909980bf063
Vic <avigdorbrandt@gmail.com>
2023-02-01 15:15:06 +0200

    set analytics ID in analytic.js

--- frontend/cocktails/src/components/analytic.js
+++ frontend/cocktails/src/components/analytic.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Analytics = () => {
   useEffect(() => {
     window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('create', process.env.GOOGLE_ANALYTICS_ID, 'auto');
     ga('send', 'pageview');
   }, []);
 
