2ba0b892c386780ba7d522f1f52ec3031308759b
Vic <avigdorbrandt@gmail.com>
2023-02-21 12:14:50 +0200

    123

--- frontend/cocktails/src/components/network_manager.jsx
+++ frontend/cocktails/src/components/network_manager.jsx
@@ -596,7 +596,7 @@ export class NetworkManager{
 		// //console.log('URL',window.location.href)
 		let url = new URL(window.location.href)
 		let host = url.host																												 
-		let result = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://craft-shake.com/get_code&prompt=consent&response_type=code&client_id=407812134261-0ocad46bbff1ur3kieba62jpfafuutgm.apps.googleusercontent.com&scope=openid%20email%20profile&access_type=offline`
+		let result = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://${host}/get_code&prompt=consent&response_type=code&client_id=407812134261-0ocad46bbff1ur3kieba62jpfafuutgm.apps.googleusercontent.com&scope=openid%20email%20profile&access_type=offline`
 		return result
 	}
 
