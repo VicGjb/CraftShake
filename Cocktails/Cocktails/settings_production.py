08d4c92ca0a4f1ad8146a2dce51acaa33f621c91
Vic <avigdorbrandt@gmail.com>
2023-02-26 23:09:02 +0200

    return default base

--- Cocktails/Cocktails/settings_production.py
+++ Cocktails/Cocktails/settings_production.py
@@ -26,20 +26,20 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('POSTGRES_URL'),
-#         'PORT': '5432',
-#   }
-  'default': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'admin',
-        'PASSWORD': 'test',
-        'HOST': 'static.95.158.90.157.clients.your-server.de',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_URL'),
         'PORT': '5432',
   }
+#   'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'test',
+#         'USER': 'admin',
+#         'PASSWORD': 'test',
+#         'HOST': 'static.95.158.90.157.clients.your-server.de',
+#         'PORT': '5432',
+#   }
 }
