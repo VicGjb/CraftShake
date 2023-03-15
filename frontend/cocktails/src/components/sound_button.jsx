c9b5c1bf1c0116874983907f0e4dc84b0c325f7b
Vic <avigdorbrandt@gmail.com>
2023-03-15 23:31:51 +0200

    123

--- frontend/cocktails/src/components/sound_button.jsx
+++ frontend/cocktails/src/components/sound_button.jsx
@@ -3,13 +3,11 @@ import "../styles/sound_button.scss";
 
 export function SoundButton() {
 //   const [playing, setPlaying] = useState(false);
-
+  const audio = new Audio('https://craftshake.s3.eu-central-1.amazonaws.com/svg/audio_2023-03-15_09-48-32.mp3');
   const handleClick = () => {
-    const audio = new Audio('https://craftshake.s3.eu-central-1.amazonaws.com/svg/audio_2023-03-15_09-48-32.ogg');
+    
     audio.play();
-    // setPlaying(true);
   };
-//${playing ? "playing" : ""}
   return (
     <div className="container">
       <button className={`big-button`} onClick={handleClick}>
