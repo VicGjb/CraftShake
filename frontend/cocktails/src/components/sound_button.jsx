5f9196c3fc15cfba012a005e0f5b790bc32d30e2
Vic <avigdorbrandt@gmail.com>
2023-03-16 00:08:35 +0200

    123

--- frontend/cocktails/src/components/sound_button.jsx
+++ frontend/cocktails/src/components/sound_button.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/sound_button.scss";
 
 export function SoundButton() {
-//   const [playing, setPlaying] = useState(false);
-  const audio = new Audio('https://craftshake.s3.eu-central-1.amazonaws.com/svg/audio_2023-03-15_09-48-32.mp3');
+  const audioRef = useRef(null);
+
   const handleClick = () => {
-    
-    audio.play();
+    audioRef.current.play();
   };
+
   return (
     <div className="container">
       <button className={`big-button`} onClick={handleClick}>
         
       </button>
+      <audio
+        ref={audioRef}
+        src="https://craftshake.s3.eu-central-1.amazonaws.com/svg/audio_2023-03-15_09-48-32.mp3"
+        onCanPlayThrough={() => {
+          audioRef.current.play();
+        }}
+      />
     </div>
   );
 }
